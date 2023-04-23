aa9767e9856de762fc8def6700b95180bc6f5fed
Akshat-SRTV <aks.sri.rt20@dypatil.edu>
2023-04-23 20:51:29 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+print("sample test case started")
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("C:/Users/aksha/Desktop/Selenium/signup.html")
+time.sleep(3)
+driver.find_element("name", "username").send_keys("akshat")
+time.sleep(3)
+# driver.find_element("name", "phone").send_keys("9076260354")
+time.sleep(3)
+driver.find_element("name", "email").send_keys("aks.sri.rt20@dypatil.edu")
+time.sleep(3)
+driver.find_element("name", "password").send_keys("abcd1234")
+time.sleep(4)
+driver.find_element("name", "submit").send_keys(Keys.ENTER)
+time.sleep(4)
+driver.close()
+print("sample test case successfully completed")
